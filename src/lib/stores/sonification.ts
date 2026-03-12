ae3d1ef20523233bb3c3de8e46b2b0afb79effb2
Peter Thomas <petethomasmusique@gmail.com>
2026-03-12 17:25:05 +0000

    sonify: create sonificationoptions

--- src/lib/stores/sonification.ts
+++ src/lib/stores/sonification.ts
@@ -3,53 +3,54 @@ import { probabilities, phases, circuit } from "./circuit/circuit";
 import { data, type Note } from "./sequencers";
 import { sequencers, divisions, bars } from "./";
 
-const defaults: { 
-    // replace existing notes, add to what's already there, 
-    strategy: 'replace' | 'add'
-} = {
+type MappingOption = 'measure' | 'probability' | 'phase' | 'random' | 'constant'
+type SonificationOptions = {
+    strategy?: 'replace' | 'add', // replace existing notes, add to what's already there
+    note?: MappingOption, // how to determine the pitch of the note
+    noteRange?: [number, number], // range to scale pitch by - 0 - 127 for MIDI
+    noteQuantize?: number, // quantize pitch to this many steps
+    amp?: MappingOption, // how to determine the amplitude of the note
+    ampRange?: [number, number], // range to scale amplitude by - 0 - 1 for MIDI velocity
+    duration?: MappingOption, // how to determine the duration of the note
+    durationRange?: [number, number], // range to scale duration by - in seconds
+    trigger?: MappingOption, // how to determine which hits trigger notes
+}
+
+const defaults: SonificationOptions = {
     strategy: 'replace',
+    note: 'constant',
+    noteRange: [48, 72],
+    noteQuantize: 1,
+    amp: 'measure',
+    ampRange: [0, 1],
+    duration: 'constant',
+    durationRange: [0.1, 1],
+    trigger: 'measure'
 }
 
 /**
  * Using the quantum circuit output, generate notes and populate the sequencers.
  * @param options 
  */
-export const sonify = (options: {} = {}) => {
-    const { strategy } = {
+export const sonify = (options: SonificationOptions = {}) => {
+    options = {
         ...defaults,
         ...options
     };
 
-    const [pbs, phs, divs, totalBars, totalSequencers] = [
-        get(probabilities),
-        get(phases),
-        get(divisions),
-        get(bars),
-        get(sequencers)
-    ];
+    const hits = get(divisions) * get(bars);
 
-    const hits = divs * totalBars;
-
-    // create measurements for all hits
     const measurements: number[][] = Array.from({ length: hits }, () => {
         circuit.run();
         return circuit.measureAll();
     });
 
-    // transform measurements to notes
-    const notes: (Note | false)[][] = measurements
-        .map((arr, i) =>
-            arr.map(measurement => measurement 
-                ? ({  
-                    position: i / divs,
-                    note: 60, // TODO: map something else
-                    amp: measurement,
-                    duration: 1 / divs 
-                }) : false
-        ));
+    const [pbs, phs, totalSequencers] = [
+        get(probabilities),
+        get(phases),
+        get(sequencers)
+    ];
 
-
-    // create sequencer data in a functional style
     data.update(data => Object.fromEntries(
         Array.from({ length: totalSequencers }, (_, s) => [
             s,
@@ -58,8 +59,7 @@ export const sonify = (options: {} = {}) => {
                 muted: false,
                 quantize: true,
                 notes: [
-                    ...notes.map(arr => arr[s % arr.length]).filter(note => note !== false) as Note[],
-                    ...(strategy === 'replace' ? [] : data[s]?.notes || [])
+                    
                 ],
                 bytebeat: "t",
             }
